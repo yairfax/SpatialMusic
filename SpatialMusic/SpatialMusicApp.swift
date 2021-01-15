ef8b0dc3e9c907b44bd4812a03a5853270f8b133
Yair <yairshop@gmail.com>
2021-01-15 10:14:15 -0500

    Initial Commit

---
+++ SpatialMusic/SpatialMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialMusicApp.swift
+//  SpatialMusic
+//
+//  Created by Yair Fax on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SpatialMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
